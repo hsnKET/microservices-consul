9abc55011bb9cf84240b198e002690d1645ad369
ketlasdev <hsn.ketlas@gmail.com>
2022-12-06 17:04:34 +0100

    feat: add entities & repos to order-service

---
+++ order-service/src/main/java/me/ketlas/orderservice/entities/ProductItem.java
@@ -0,0 +1,30 @@
+package me.ketlas.orderservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.ketlas.orderservice.model.Product;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long productId;
+    @Transient
+    private Product product;
+    private double price;
+    private int quantity;
+    private double discount;
+    @ManyToOne
+    private Order order;
+
+
+}
