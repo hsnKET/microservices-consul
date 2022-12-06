3b2ef639b79158d1e1b38e26fb5eb03579d11ca8
ketlasdev <hsn.ketlas@gmail.com>
2022-12-06 02:54:12 +0100

    feat: customer service configuration

---
+++ customer-service/src/main/java/me/ketlas/customerservice/entities/Customer.java
@@ -0,0 +1,16 @@
+package me.ketlas.customerservice.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+}
