e180d74354d98f19f825e535616fd25e936799f1
ketlasdev <hsn.ketlas@gmail.com>
2022-12-06 11:29:49 +0100

    build: add actuator dependency

--- customer-service/src/main/java/me/ketlas/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/me/ketlas/customerservice/CustomerServiceApplication.java
@@ -1,7 +1,13 @@
 package me.ketlas.customerservice;
 
+import me.ketlas.customerservice.entities.Customer;
+import me.ketlas.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
@@ -9,5 +15,18 @@ public class CustomerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class,args);
+    }
+
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository){
+        return args -> {
+            Arrays.asList("hsn","jwd","hmz","brhm")
+                    .forEach(name ->{
+                        customerRepository.save(Customer.builder()
+                                .email(name.concat("@gmail.com"))
+                                .name(name)
+                                .build());
+                    });
+        };
     }
 }
