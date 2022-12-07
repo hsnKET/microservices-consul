5cb3bd73b47f9ec2f7f539af0ab27f8a8bdf3f54
ketlasdev <hsn.ketlas@gmail.com>
2022-12-07 16:06:29 +0100

    feat: configure vault

--- billing-service/src/main/java/me/ketlas/web/BillingRestController.java
+++ billing-service/src/main/java/me/ketlas/web/BillingRestController.java
@@ -1,10 +1,13 @@
 package me.ketlas.web;
 
 import me.ketlas.config.ConsulConfiguration;
+import me.ketlas.config.VaultConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 
 @RestController
@@ -13,8 +16,11 @@ public class BillingRestController {
     @Autowired
     private ConsulConfiguration consulConfiguration;
 
+    @Autowired
+    private VaultConfiguration vaultConfiguration;
+
     @GetMapping("/myConfig")
-    public ConsulConfiguration getMyConfig(){
-        return this.consulConfiguration;
+    public Map<String,Object> getMyConfig(){
+        return Map.of("consult_config",consulConfiguration,"vault_config",vaultConfiguration);
     }
 }
