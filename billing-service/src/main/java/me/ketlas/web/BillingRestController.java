5cbccf3022af7f260c76872b12babf324dc98751
ketlasdev <hsn.ketlas@gmail.com>
2022-12-07 14:33:08 +0100

    refactor: migrate to consul class loader configuration

--- billing-service/src/main/java/me/ketlas/web/BillingRestController.java
+++ billing-service/src/main/java/me/ketlas/web/BillingRestController.java
@@ -1,24 +1,20 @@
 package me.ketlas.web;
 
-import org.springframework.beans.factory.annotation.Value;
+import me.ketlas.config.ConsulConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
 @RestController
-@RefreshScope
 public class BillingRestController {
 
-    @Value("${token.refreshTokenTimeout}")
-    private Long refreshTokenTimeout;
-
-    @Value("${token.accessTokenTimeout}")
-    private Long accessTokenTimeout;
+    @Autowired
+    private ConsulConfiguration consulConfiguration;
 
     @GetMapping("/myConfig")
-    public Map<String,Object> getMyConfig(){
-        return Map.of("refreshTokenTimeout",refreshTokenTimeout,"accessTokenTimeout",accessTokenTimeout);
+    public ConsulConfiguration getMyConfig(){
+        return this.consulConfiguration;
     }
 }
