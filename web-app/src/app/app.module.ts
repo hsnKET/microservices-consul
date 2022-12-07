e46acbc976ec1fc76323eb31ae7f20a6d41cfe03
ketlasdev <hsn.ketlas@gmail.com>
2022-12-07 19:11:18 +0100

    feat: add products component

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import {RouterModule, Routes} from "@angular/router";
+
+
+const  appRoutes:Routes = [
+  {path:"products",component:ProductsComponent},
+  {path:"",redirectTo:"products",pathMatch:"full"}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
